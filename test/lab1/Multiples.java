870f9a09bea3ab61c1129b44e8ce1b5ea4d18d00
Grace1563 <gracelin1563@gmail.com>
2025-09-09 01:06:19 -0400

    update

--- test/lab1/Multiples.java
+++ test/lab1/Multiples.java
@@ -2,18 +2,18 @@ package lab1;
 //test
 public class Multiples {
     public static void main(String[] args) {
-        int count = multiples();
+        int count = multiples(1000, 3, 5);
         System.out.println(count);
 
     }
 
-    private static int multiples() {
+    static int multiples(int n, int a, int b) {
         int count = 0;
-        for (int i = 1; i < 1000; i++) {
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
+        for (int i = 1; i < n; i++) {
+            boolean divisibleBya = i % a == 0;
+            boolean divisibleByb = i % b == 0;
 
-            if (divisibleBy3 && divisibleBy5) {
+            if (divisibleBya && divisibleByb) {
                 count++;
             }
         }
