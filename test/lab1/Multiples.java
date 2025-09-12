797106b44fbc92af3e7edd08c61a8990be4817d9
Grace1563 <gracelin1563@gmail.com>
2025-09-12 17:00:02 -0400

    update

--- test/lab1/Multiples.java
+++ test/lab1/Multiples.java
@@ -17,4 +17,17 @@ public class Multiples {
         }
         return count;
     }
+
+    static int multiples() {
+        int count = 0;
+        for (int i = 1; i < 1000; i++) {
+            boolean divisibleBy3 = i % 3 == 0;
+            boolean divisibleBy5 = i % 5 == 0;
+
+            if (divisibleBy3 || divisibleBy5) {
+                count++;
+            }
+        }
+        return count;
+    }
 }
